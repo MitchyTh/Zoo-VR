ccc6821ab6ee2ce0b37835a21daf69f243c669c7
Jake <user@FIH-COM23a111.fsu.edu>
2026-09-17 12:43:19 -0400

    Starts the welfare system

---
+++ Assets/Scripts/HabitatStates.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class HabitatStates
+{
+    public int minWelfare = 0;
+    public int maxWelfare = 0;
+    public int minShelterScore = 0;
+    public int maxShelterScore = 0;
+
+
+    public int shelterScore;
+    
+
+}
